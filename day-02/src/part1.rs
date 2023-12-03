dc7e76bb33e88f30dc35c74a2c8126394c8611d9
Nicolas DUBIEN <github@dubien.org>
2023-12-03 17:10:40 +0000

    Day 02 - Better implem

--- day-02/src/part1.rs
+++ day-02/src/part1.rs
@@ -3,20 +3,12 @@ use crate::parser;
 pub fn run(corpus: &str) -> u32 {
     let games = corpus.split('\n').map(parser::parse_game);
     games
-        .map(|parsed| {
-            parsed
-                .map(|game| {
-                    if game
-                        .turns
-                        .iter()
-                        .all(|turn| turn.red <= 12 && turn.green <= 13 && turn.blue <= 14)
-                    {
-                        game.id
-                    } else {
-                        0
-                    }
-                })
-                .unwrap_or_default()
+        .filter_map(|parsed| parsed.ok())
+        .filter(|game| {
+            game.turns
+                .iter()
+                .all(|turn| turn.red <= 12 && turn.green <= 13 && turn.blue <= 14)
         })
+        .map(|game| game.id)
         .sum()
 }
--- day-02/src/part2.rs
+++ day-02/src/part2.rs
@@ -1,23 +1,14 @@
 use crate::parser;
 
 pub fn run(corpus: &str) -> u32 {
-    let games = corpus.split('\n').map(parser::parse_game);
-    games
-        .map(|parsed| -> u32 {
-            let parsed = parsed.unwrap();
-            let max_reds = parsed.turns.iter().map(|t| t.red).max().unwrap_or_default();
-            let max_greens = parsed
-                .turns
-                .iter()
-                .map(|t| t.green)
-                .max()
-                .unwrap_or_default();
-            let max_blues = parsed
-                .turns
-                .iter()
-                .map(|t| t.blue)
-                .max()
-                .unwrap_or_default();
+    corpus
+        .split('\n')
+        .map(parser::parse_game)
+        .filter_map(|parsed| parsed.ok())
+        .map(|game| -> u32 {
+            let max_reds = game.turns.iter().map(|t| t.red).max().unwrap_or_default();
+            let max_greens = game.turns.iter().map(|t| t.green).max().unwrap_or_default();
+            let max_blues = game.turns.iter().map(|t| t.blue).max().unwrap_or_default();
             max_reds * max_greens * max_blues
         })
         .sum()
